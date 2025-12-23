9fd6be449d766f7312df62fd8b58a1b7e536cba6
Aniket Panchal <aniket.vilas.panchal@gmail.com>
2025-12-23 13:51:53 +0530

    added the sender and getter part

--- app/src/components/window/CustomMessageList.tsx
+++ app/src/components/window/CustomMessageList.tsx
@@ -1,26 +1,40 @@
-
-
-import React from 'react'
-
-import { useChannelStateContext } from "stream-chat-react";
+import React from "react";
+import {
+  useChannelStateContext,
+  useChatContext,
+} from "stream-chat-react";
 
 const CustomMessageList = () => {
-    const { messages } = useChannelStateContext();
-    return (
-        <div className="flex flex-col gap-2 p-4">
-            {messages!.map((msg) => (
-                <div
-                    key={msg.id}
-                    className={`max-w-xs p-3 rounded-lg ${msg.user?.id === "currentUserId"
-                        ? "bg-blue-500 text-white self-end"
-                        : "bg-gray-200 text-black self-start"
-                        }`}
-                >
-                    <p className="text-sm">{msg.text}</p>
-                </div>
-            ))}
-        </div>
-    );
-}
+  const { messages } = useChannelStateContext();
+  const { client } = useChatContext();
 
-export default CustomMessageList
+  const currentUserId = client.userID;
+
+  return (
+    <div className="w-full max-w-2xl  mx-auto bg-gray-100 flex flex-col gap-3 p-4 rounded-md">
+      {messages!.map((msg) => {
+        const isSender = msg.user?.id === currentUserId;
+
+        return (
+          <div
+            key={msg.id}
+            className={`flex ${isSender ? "justify-end" : "justify-start"}`}
+          >
+            <div
+              className={`max-w-[70%] px-4 py-2 rounded-lg text-sm
+                ${
+                  isSender
+                    ? "bg-blue-500 text-white rounded-br-none"
+                    : "bg-white text-black rounded-bl-none"
+                }`}
+            >
+              {msg.text}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CustomMessageList;
